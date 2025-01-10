9d09ba762f06c6317e632cc4297d8e0c87ac982f
Omar Ledesma <joseomar.ledagui@gmail.com>
2025-01-10 14:39:56 +0000

    complete mobileNav & navLinks

---
+++ app/(root)/collection/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Collection = () => {
+  return (
+    <div>page</div>
+  )
+}
+
+export default Collection
--- components/navigartion/navbar/NavLinks.tsx
+++ components/navigartion/navbar/NavLinks.tsx
@@ -5,9 +5,12 @@ import { sidebarLinks } from "@/constants";
 import { usePathname } from "next/navigation";
 import Link from "next/link";
 import Image from "next/image";
+import { cn } from "@/lib/utils";
+import { SheetClose } from "@/components/ui/sheet";
 
 const NavLinks = ({ isMobileNav = false }: { isMobileNav?: boolean }) => {
   const pathname = usePathname();
+  const userId = 1;
 
   return (
     <>
@@ -16,17 +19,28 @@ const NavLinks = ({ isMobileNav = false }: { isMobileNav?: boolean }) => {
           (pathname.includes(item.route) && item.route.length > 1) ||
           pathname === item.route;
 
+          if (item.route === "/profile") {
+            if (userId) item.route = `${item.route}/${userId}`;
+            else return null;
+          }
+
         const linkComponent = (
-          <Link href={item.route} key={item.label} className="invert-colors">
-            <Image src={item.imgURL} alt={item.label} width={20} height={20} />
-            <p>{item.label}</p>
+          <Link href={item.route} key={item.label} className={cn(isActive ? 'primary-gradient rounded-lg text-light900' : 'text-dark300_light900 bg-transparent', "flex justify-start items-center gap-4 p-4")}>
+            <Image src={item.imgURL} alt={item.label} width={20} height={20} className={cn("invert-colors",{"invert-color" : !isActive})} />
+            <p className={cn(isActive ? 'font-bold' : 'font-light', !isMobileNav && 'max-lg:hidden')}>{item.label}</p>
           </Link>
         );
 
-        return linkComponent;
+        return isMobileNav ? (
+          <SheetClose asChild key={item.route}>
+            {linkComponent}
+          </SheetClose>
+        ): (<React.Fragment key={item.route}>
+          {linkComponent}
+        </React.Fragment>)
       })}
     </>
-  )
-}
+  );
+};
 
 export default NavLinks;
